43adb3ee3876cc5da9431ce45525336fed652cff
houxiaolei <1206972623@qq.com>
2017-05-21 22:13:00 +0800

    first commit

---
+++ src/main/java/com/ujn/hxl/inter/IUserOperation.java
@@ -0,0 +1,10 @@
+package com.ujn.hxl.inter;
+
+import com.ujn.hxl.model.User;
+
+/**
+ * Created by lss on 2017/5/21.
+ */
+public interface IUserOperation {
+    public User selectUserByID(int id);
+}
---
+++ src/test/java/com/ujn/hxl/test/Test.java
@@ -0,0 +1,62 @@
+package com.ujn.hxl.test;
+
+import com.ujn.hxl.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.Reader;
+
+/**
+ * Created by lss on 2017/5/21.
+ */
+public class Test {
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+    static {
+        try {
+            reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static SqlSessionFactory getSession(){
+        return sqlSessionFactory;
+    }
+
+    public static void test1(){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            User user = (User)session.selectOne("com.ujn.hxl.models.UserMapper.selectUserByID",1);
+            System.out.println(user.getUserName());
+            System.out.println(user.getUserAddress());
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(session!=null){
+                session.close();
+            }
+        }
+    }
+
+    public static void test2(){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            User user = (User)session.selectOne("com.ujn.hxl.models.UserMapper.selectUserByID",1);
+            System.out.println(user.getUserName());
+            System.out.println(user.getUserAddress());
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(session!=null){
+                session.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
